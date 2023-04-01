33d62bbfa1e1c92b614e7538a65d5794c5b5df3e
VVIDALSI <106885575+VVIDALSI@users.noreply.github.com>
2023-04-01 13:34:06 -0500

    Referencias

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,7 +6,9 @@ import stlSkill from "./Skill.module.css";
 export default function Profile() {
     return (
         <div className={stlSkill.containerSkill}>
-
+            <ul>
+                <li>Añade en el menu "Informacion personal"</li>
+            </ul>
         </div>
     );
 }
